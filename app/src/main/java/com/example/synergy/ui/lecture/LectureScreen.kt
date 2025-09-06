59240634a812be7f550e3cd0e575e6c1d284e592
GwonDongHyeon21 <GwonDH21@gmail.com>
2025-09-07 06:21:03 +0900

    feat: 강의 화면 빈 상태로 추가

--- app/src/main/java/com/example/synergy/ui/lecture/LectureScreen.kt
+++ app/src/main/java/com/example/synergy/ui/lecture/LectureScreen.kt
@@ -1,7 +1,24 @@
 package com.example.synergy.ui.lecture
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.example.synergy.R
 
 @Composable
 fun LectureScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f),
+            contentDescription = null
+        )
+    }
 }
