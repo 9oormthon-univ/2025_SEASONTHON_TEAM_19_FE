049485d94ba3aca19716e34ee9f1e9cec51c9f65
GwonDongHyeon21 <GwonDH21@gmail.com>
2025-09-06 21:00:07 +0900

    feat: 강의 탭, 유저 탭 빈 화면 추가

---
+++ app/src/main/java/com/example/synergy/ui/lecture/LectureScreen.kt
@@ -0,0 +1,7 @@
+package com.example.synergy.ui.lecture
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LectureScreen() {
+}
---
+++ app/src/main/java/com/example/synergy/ui/lecture/LectureViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.synergy.ui.lecture
+
+import androidx.lifecycle.ViewModel
+
+class LectureViewModel : ViewModel() {
+}
